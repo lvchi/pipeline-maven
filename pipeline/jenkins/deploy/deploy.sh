733bf15549f73eb4d04b13c21ad8aa74368c6a2f
ChiLam <lvchi.dtvt@gmail.com>
2020-02-23 13:33:12 +0700

    done

--- pipeline/jenkins/deploy/deploy.sh
+++ pipeline/jenkins/deploy/deploy.sh
@@ -4,7 +4,7 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i $WORKSPACE/pipeline/jenkins/deploy/key ./jenkins/deploy/docker-compose.yml prod-user@10.130.9.253:/tmp/docker-compose.yml
-scp -i $WORKSPACE/pipeline/jenkins/deploy/key /tmp/.auth prod-user@10.130.9.253:/tmp/.auth
-scp -i $WORKSPACE/pipeline/jenkins/deploy/key ./jenkins/deploy/publish prod-user@10.130.9.253:/tmp/publish
-ssh -i $WORKSPACE/pipeline/jenkins/deploy/key prod-user@10.130.9.253 "/tmp/publish"
+scp -i /home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven/pipeline/jenkins/deploy/key ./jenkins/deploy/docker-compose.yml prod-user@10.130.9.253:/tmp/docker-compose.yml
+scp -i /home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven/pipeline/jenkins/deploy/key /tmp/.auth prod-user@10.130.9.253:/tmp/.auth
+scp -i /home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven/pipeline/jenkins/deploy/key ./jenkins/deploy/publish prod-user@10.130.9.253:/tmp/publish
+ssh -i /home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven/pipeline/jenkins/deploy/key prod-user@10.130.9.253 "/tmp/publish"
