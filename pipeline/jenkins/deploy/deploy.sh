82e50f5b5501e02caa6794391502e5dfdad70315
ChiLam <lvchi.dtvt@gmail.com>
2020-02-23 13:22:52 +0700

    done

--- pipeline/jenkins/deploy/deploy.sh
+++ pipeline/jenkins/deploy/deploy.sh
@@ -4,7 +4,7 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i $WORKSPACE/pipeline/jenkins/deploy/key ./jenkins/deploy/docker-compose.yml prod-user@linuxfacilito.online:/tmp/docker-compose.yml
-scp -i $WORKSPACE/pipeline/jenkins/deploy/key /tmp/.auth prod-user@linuxfacilito.online:/tmp/.auth
-scp -i $WORKSPACE/pipeline/jenkins/deploy/key ./jenkins/deploy/publish prod-user@linuxfacilito.online:/tmp/publish
-ssh -i $WORKSPACE/pipeline/jenkins/deploy/key prod-user@linuxfacilito.online "/tmp/publish"
+scp -i $WORKSPACE/pipeline/jenkins/deploy/key ./jenkins/deploy/docker-compose.yml prod-user@10.130.9.253:/tmp/docker-compose.yml
+scp -i $WORKSPACE/pipeline/jenkins/deploy/key /tmp/.auth prod-user@10.130.9.253:/tmp/.auth
+scp -i $WORKSPACE/pipeline/jenkins/deploy/key ./jenkins/deploy/publish prod-user@10.130.9.253:/tmp/publish
+ssh -i $WORKSPACE/pipeline/jenkins/deploy/key prod-user@10.130.9.253 "/tmp/publish"
