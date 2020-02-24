7b5d7683630aa8082922983122d75889db2cb67d
Lam Van <chi.lam.cto@gmail.com>
2020-02-24 13:36:35 +0700

    done

--- pipeline/jenkins/push/push.sh
+++ pipeline/jenkins/push/push.sh
@@ -7,7 +7,7 @@ echo "********************"
 IMAGE="maven-project"
 
 echo "** Logging in ***"
-docker login -u chilv -p "toiyeuem1990"
+docker login -u chilv -p $PASS
 echo "*** Tagging image ***"
 docker tag $IMAGE:$BUILD_TAG chilv/$IMAGE:$BUILD_TAG
 echo "*** Pushing image ***"
