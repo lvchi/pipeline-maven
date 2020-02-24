d0d1ad2b05dce8d8911fb714dd16fd404add61f8
Lam Van <chi.lam.cto@gmail.com>
2020-02-24 10:56:06 +0700

    don

--- pipeline/jenkins/build/mvn.sh
+++ pipeline/jenkins/build/mvn.sh
@@ -4,6 +4,8 @@ echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
 
+pwd
+
 WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
 
 docker run --rm -v $WORKSPACE/pipeline/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
