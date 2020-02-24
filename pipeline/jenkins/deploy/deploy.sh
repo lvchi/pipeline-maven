aa0e4576a1bad8545b095baf5b26a2b27716376f
Lam Van <chi.lam.cto@gmail.com>
2020-02-24 13:54:41 +0700

    done

--- pipeline/jenkins/deploy/deploy.sh
+++ pipeline/jenkins/deploy/deploy.sh
@@ -5,8 +5,8 @@ echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
 chmod 400 /var/jenkins_home/workspace/pipeline-docker-maven/pipeline/jenkins/deploy/key
-scp -i /var/jenkins_home/workspace/pipeline-docker-maven/pipeline/jenkins/deploy/key ./pipeline/jenkins/deploy/docker-compose.yml prod-user@10.130.9.253:/tmp/docker-compose.yml
-scp -i /var/jenkins_home/workspace/pipeline-docker-maven/pipeline/jenkins/deploy/key /tmp/.auth prod-user@10.130.9.253:/tmp/.auth
-scp -i /var/jenkins_home/workspace/pipeline-docker-maven/pipeline/jenkins/deploy/key ./pipeline/jenkins/deploy/publish prod-user@10.130.9.253:/tmp/publish
-ssh -i /var/jenkins_home/workspace/pipeline-docker-maven/pipeline/jenkins/deploy/key prod-user@10.130.9.253 "chmod +x /tmp/publish"
-ssh -i /var/jenkins_home/workspace/pipeline-docker-maven/pipeline/jenkins/deploy/key prod-user@10.130.9.253 "/tmp/publish"
+scp -i /var/jenkins_home/workspace/pipeline-docker-maven/pipeline/jenkins/deploy/key ./pipeline/jenkins/deploy/docker-compose.yml prod-user@10.130.9.11:/tmp/docker-compose.yml
+scp -i /var/jenkins_home/workspace/pipeline-docker-maven/pipeline/jenkins/deploy/key /tmp/.auth prod-user@10.130.9.11:/tmp/.auth
+scp -i /var/jenkins_home/workspace/pipeline-docker-maven/pipeline/jenkins/deploy/key ./pipeline/jenkins/deploy/publish prod-user@10.130.9.11:/tmp/publish
+ssh -i /var/jenkins_home/workspace/pipeline-docker-maven/pipeline/jenkins/deploy/key prod-user@10.130.9.11 "chmod +x /tmp/publish"
+ssh -i /var/jenkins_home/workspace/pipeline-docker-maven/pipeline/jenkins/deploy/key prod-user@10.130.9.11 "/tmp/publish"
